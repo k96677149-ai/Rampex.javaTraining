41a64ff483030b1f45c0bcd1e1f57d7ecd304d0b
keertana <k96677149@gmail.com>
2026-07-03 12:21:37 +0530

    program to reverse array elements

---
+++ src/Day_7/Home_task/reverse_array.java
@@ -0,0 +1,28 @@
+package Day_7.Home_task;
+
+import java.util.Scanner;
+
+public class reverse_array {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements:");
+
+        for(int i = 0; i < size; ++i) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i = size - 1; i >= 0; --i) {
+            if (i == size - 1) {
+                System.out.print("[" + arr[i]);
+            } else {
+                System.out.print("," + arr[i]);
+            }
+        }
+
+        System.out.print("]");
+    }
+}
