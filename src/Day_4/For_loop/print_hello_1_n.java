ac085e822bb13fe208d53115765d9b70465830cb
keertana <k96677149@gmail.com>
2026-07-03 11:37:40 +0530

    program to print hello 1-n times

---
+++ src/Day_4/For_loop/print_hello_1_n.java
@@ -0,0 +1,11 @@
+package Day_4.For_loop;
+
+public class print_hello_1_n {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; ++i) {
+            System.out.println("Hello," + i);
+        }
+
+    }
+}
+
