3b4eeb54d5a94fbc5073de55cfb4eba1d602d117
keertana <k96677149@gmail.com>
2026-07-03 15:56:48 +0530

    program to check amstrong number

---
+++ src/Day_11/Class_task/armstrong_number.java
@@ -0,0 +1,30 @@
+package Day_11.Class_task;
+
+public class armstrong_number {
+
+    public static void main(String[] args) {
+        System.out.println(checkamstrong(154));
+    }
+
+    public static boolean checkamstrong(int num) {
+        int digit = 0;
+        int temp = num;
+
+        int Originalnum;
+        for(Originalnum = num; temp != 0; ++digit) {
+            temp /= 10;
+        }
+
+        int rem = 0;
+
+        int sum;
+        for(sum = 0; num != 0; num /= 10) {
+            rem = num % 10;
+            sum += (int)Math.pow((double)rem, (double)digit);
+        }
+
+        System.out.println(sum);
+        return Originalnum == sum;
+    }
+}
+
