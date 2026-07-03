5dc4eacae4305ebe6d3d757e7541cfc060dfa663
keertana <k96677149@gmail.com>
2026-07-03 12:18:59 +0530

    sample program 1 for arrays

---
+++ src/Day_7/Class_task/array_1.java
@@ -0,0 +1,17 @@
+package Day_7.Class_task;
+
+import java.util.Arrays;
+
+public class array_1 {
+
+        public static void main(String[] args) {
+            int[] arr = new int[5];
+
+            for (int i = 0; i <= 4; ++i) {
+                arr[i] = (1 + i) * 1;
+            }
+
+            System.out.println("Array: " + Arrays.toString(arr));
+        }
+    }
+
