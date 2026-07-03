7f8c42a0baa088636dadefd57d783f02a481520d
keertana <k96677149@gmail.com>
2026-07-03 12:19:46 +0530

    program to count odd or even

---
+++ src/Day_7/Home_task/count_even_odd.java
@@ -0,0 +1,21 @@
+package Day_7.Home_task;
+
+public class count_even_odd {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int count = 0;
+        int count1 = 0;
+
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] % 2 == 0) {
+                ++count;
+            } else {
+                ++count1;
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(count1);
+    }
+}
