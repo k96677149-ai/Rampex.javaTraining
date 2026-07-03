6ae6e6becd2aa0ea47d52e2f8e0bfab87aba0189
keertana <k96677149@gmail.com>
2026-07-03 10:37:21 +0530

    program to print largest of three numbers

---
+++ src/Day_2/if_else/largest_of_three_number.java
@@ -0,0 +1,17 @@
+package Day_2.if_else;
+
+public class largest_of_three_number {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 15;
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest number.");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest number.");
+        } else {
+            System.out.println(c + " is the largest number.");
+        }
+    }
+}
+
