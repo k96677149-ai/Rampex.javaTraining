fb5c2c7bfa60fcaccaa747604912ea450344d032
keertana <k96677149@gmail.com>
2026-07-03 11:39:22 +0530

    program to print the multiplication tables

---
+++ src/Day_4/For_loop/tables.java
@@ -0,0 +1,18 @@
+package Day_4.For_loop;
+
+import java.util.Scanner;
+
+public class tables {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the which tables you need to show: ");
+        int a = scanner.nextInt();
+
+        for(int i = 1; i <= 20; ++i) {
+            System.out.println(i + " x " + a + " = " + i * a);
+        }
+
+        scanner.close();
+    }
+}
+
