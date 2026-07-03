0663c48962a7ffad2f98447794175ef828a89ec3
keertana <k96677149@gmail.com>
2026-07-03 11:39:36 +0530

    sample program

---
+++ src/Day_4/For_loop/sample.java
@@ -0,0 +1,11 @@
+package Day_4.For_loop;
+
+public class sample {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; ++i) {
+            System.out.println("Hello");
+        }
+
+    }
+}
