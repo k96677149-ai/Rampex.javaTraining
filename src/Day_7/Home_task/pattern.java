1678fe4ff73adc3c8253aee5c10cb674a7ac23d2
keertana <k96677149@gmail.com>
2026-07-05 10:32:17 +0530

    program for pattern printing

--- src/Day_7/Home_task/pattern.java
+++ src/Day_7/Home_task/pattern.java
@@ -3,20 +3,20 @@ package Day_7.Home_task;
 public class pattern {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; ++i) {
-            for (int j = i; j <= 5; ++j) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 5; j++) {
                 System.out.print(j);
             }
 
             System.out.println();
         }
 
-        for (int i = 4; i >= 1; --i) {
-            for (int j = i; i <= 5; ++j) {
+        for (int i = 5-1; i >= 1; i--) {
+            for (int j = i; j <= 5; j++) {
                 System.out.print(j);
             }
 
             System.out.println();
         }
-    }
+        }
 }
