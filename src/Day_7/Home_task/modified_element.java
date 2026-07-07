18d9cf7de5bd4fadf91baaf25a07888cadebb0de
keertana <k96677149@gmail.com>
2026-07-07 21:41:00 +0530

    program for modify the array if element negative replace zero

--- src/Day_7/Home_task/modified_element.java
+++ src/Day_7/Home_task/modified_element.java
@@ -6,7 +6,7 @@ public class modified_element {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, -5, 6, -7};
 
-        for (int i = 0; i < arr.length; ++i) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 0) {
                 arr[i] = 0;
             }
