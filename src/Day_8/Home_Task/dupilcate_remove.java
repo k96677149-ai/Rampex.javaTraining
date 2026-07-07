c53d0ea11792fdf8a214c9905790ae65468b4d7c
keertana <k96677149@gmail.com>
2026-07-07 22:11:07 +0530

    program for remove duplicate and print

--- src/Day_8/Home_Task/dupilcate_remove.java
+++ src/Day_8/Home_Task/dupilcate_remove.java
@@ -6,26 +6,17 @@ import java.util.Scanner;
 public class dupilcate_remove {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr2 = new int[5];
-        System.out.println("Enter the size:");
-        int size = sc.nextInt();
-        int[] arr = new int[size];
-        System.out.println("Enter the arry element:");
-
-        for(int i = 0; i < size; ++i) {
-            arr[i] = sc.nextInt();
-        }
-
-        for(int i = 0; i < size; ++i) {
-            for(int j = 0; j <= i; ++j) {
-                if (arr[i] == arr[j] && i != j) {
-                    arr2[i] = arr[i];
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arr2));
+        int[] arr = {1,2,2,3,4,4,5};
+       for(int i=0;i<arr.length;i++){
+           boolean isDuplicate = false;
+           for(int j=0;j<i;j++){
+               if(arr[i]==arr[j]){
+                   isDuplicate = true;
+               }
+           }
+           if(!isDuplicate){
+               System.out.print(arr[i] + " ");
+           }
+       }
     }
 }
