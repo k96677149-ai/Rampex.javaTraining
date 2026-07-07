76747ebb0ec2aeea8f3f33d54f428d0aadf05dad
keertana <k96677149@gmail.com>
2026-07-07 14:52:55 +0530

    program for pattern

--- src/Day_5/nested_for.java
+++ src/Day_5/nested_for.java
@@ -4,7 +4,7 @@ public class nested_for {
 
     public static void main(String[] args) {
         for(int i = 0; i <= 5; ++i) {
-            for(int j = 1; j <= 3; ++j) {
+            for(int j = 1; j <= 5; ++j) {
                 System.out.print(j);
             }
 
