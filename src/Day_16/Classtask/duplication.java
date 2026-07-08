96f389d705c041d5f5d82098221dc7c3e63bd523
keertana <k96677149@gmail.com>
2026-07-08 19:21:20 +0530

    program for duplicate

---
+++ src/Day_16/Classtask/duplication.java
@@ -0,0 +1,20 @@
+package Day_16.Classtask;
+
+public class duplication {
+    public static void main(String[] args) {
+        System.out.println(Isduplicate("carrace"));
+
+    }
+    public static boolean Isduplicate(String name){
+        String t = "car";
+        while(true){
+            if(name.contains(String.valueOf(t)) == false){
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
+
