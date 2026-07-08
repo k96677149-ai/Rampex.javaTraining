c56c71ba248b9c9d2abd6a475f13011165e53a83
keertana <k96677149@gmail.com>
2026-07-08 22:32:22 +0530

    program for remove duplicate in HashSet

---
+++ src/Day_16/Hometask/RemoveDuplicateHashset.java
@@ -0,0 +1,18 @@
+package Day_16.Hometask;
+
+import java.util.HashSet;
+
+public class RemoveDuplicateHashset {
+    public static void main(String[] args){
+        HashSet<Integer> num = new HashSet<>();
+        num.add(1);
+        num.add(2);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        num.add(4);
+        num.add(5);
+        System.out.println(num);
+
+    }
+}
