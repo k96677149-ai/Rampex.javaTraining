49eba75716b4f9c663a10655ec23408e1676bc7c
keertana <k96677149@gmail.com>
2026-07-13 22:19:57 +0530

    program for swiggy application

---
+++ src/Day_16/Hometask/Swiggy.java
@@ -0,0 +1,26 @@
+package Day_16.Hometask;
+
+public class Swiggy {
+    public static void main(String[] args){
+
+    }
+}
+class Restaurant{
+    private int rid;
+    private String rname;
+    Restaurant(){
+
+    }
+}
+class Customer{
+    private int cid;
+    private String cname;
+}
+class Deliverypartner{
+    private int dpid;
+    private String dpname;
+}
+class Order{
+    private int oid;
+    private String oname;
+}
