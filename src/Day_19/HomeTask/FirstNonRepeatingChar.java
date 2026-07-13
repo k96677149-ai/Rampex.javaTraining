becc96f7b8a6370d65fd14a6813877894106e379
keertana <k96677149@gmail.com>
2026-07-13 22:10:02 +0530

    program for first Non repeating char in string

---
+++ src/Day_19/HomeTask/FirstNonRepeatingChar.java
@@ -0,0 +1,23 @@
+package Day_19.HomeTask;
+import java.util.ArrayList;
+import java.util.Collections;
+public class FirstNonRepeatingChar {
+    public static void main(String[] args){
+        String str = "proframming";
+        ArrayList<Character> list = new ArrayList<>();
+      for(int i=0;i<str.length();i++){
+          list.add(str.charAt(i));
+      }
+      for(Character ch:list){
+          if(Collections.frequency(list,ch) == 1){
+              System.out.print(ch);
+              break;
+          }
+
+      }
+
+
+
+
+    }
+}
