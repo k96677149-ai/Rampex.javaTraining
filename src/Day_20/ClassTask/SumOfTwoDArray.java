f16004a6431e9a43a6aab46ba23b1c3e9a57ded4
keertana <k96677149@gmail.com>
2026-07-13 22:16:08 +0530

    program for 2D array

---
+++ src/Day_20/ClassTask/SumOfTwoDArray.java
@@ -0,0 +1,14 @@
+package Day_20.ClassTask;
+
+public class SumOfTwoDArray {
+    public static void main(String[] args){
+        int sum =0;
+        int[][] arr = {{1,2,3,4},{5,6,7,8}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                sum += arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
